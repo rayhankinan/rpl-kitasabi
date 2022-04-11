13fa298861d7cabd2770624a20765ab1a84aec62
rayhankinan <rayhankinan@gmail.com>
2022-04-12 01:02:34 +0700

    Add DDL

--- src/models/akunModels.py
+++ src/models/akunModels.py
@@ -1,4 +1,14 @@
 from db import mysql
 
 class Akun:
-    pass
+    def __init__(self, email, noTelp, namaDepan, namaBelakang, username, password, foto):
+        self.email = email
+        self.noTelp = noTelp
+        self.namaDepan = namaDepan
+        self.namaBelakang = namaBelakang
+        self.username = username
+        self.password = password
+        self.foto = foto
+
+    def create():
+        pass
