b4a74ec1387469b140c9b41c109f9406a981a02d
rayhankinan <rayhankinan@gmail.com>
2022-04-14 20:12:09 +0700

    Fix session

--- src/main.py
+++ src/main.py
@@ -1,9 +1,7 @@
 from application import app
 from routes.akunRoutes import akunRoutes
-
-def initialize_app():
+    
+if __name__ == "__main__":
     app.register_blueprint(akunRoutes, url_prefix="/akun")
 
-if __name__ == "__main__":
-    initialize_app()
     app.run(port=3000, debug=True)
--- src/middleware/session.py
+++
@@ -1,8 +0,0 @@
-from flask_session import Session
-
-from config import sessionConfig
-from application import app
-
-app.config.from_object(sessionConfig)
-
-sess = Session(app)
