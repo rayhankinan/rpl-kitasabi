1d44ef6ae2f3d0e411ef19a1a8a5ff6bf1fac9ee
rayhankinan <rayhankinan@gmail.com>
2022-04-17 06:10:28 +0700

    Menambahkan transaksi

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from application import app
 from routes.akunRoutes import akunRoutes
 from routes.transaksiRoutes import transaksiRoutes
-    
+
 if __name__ == "__main__":
     app.register_blueprint(akunRoutes, url_prefix="/akun")
     app.register_blueprint(transaksiRoutes, url_prefix="/transaksi")
