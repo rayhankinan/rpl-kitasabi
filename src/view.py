902582ed30b8789a4979e3bbba172d0c5a3fdf8b
rayhankinan <rayhankinan@gmail.com>
2022-04-19 00:40:53 +0700

    Delete view

--- src/view.py
+++
@@ -1,8 +0,0 @@
-from views import pageController
-from PyQt6.QtWidgets import QApplication
-import sys
-
-if __name__ == "__main__":
-    window = QApplication(sys.argv)
-    view = pageController.PageController()
-    sys.exit(window.exec())
